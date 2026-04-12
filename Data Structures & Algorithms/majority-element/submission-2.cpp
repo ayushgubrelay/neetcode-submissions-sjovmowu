956d7b16bb2851b9e671ffcac94902fad85406e9
ayush kumar gubrelay <ayushgubrele4@gmail.com>
2026-04-12 21:47:22 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        // first approach
+        // int n = nums.size();
+        // unordered_map<int,int>mp;
+        // for(int i = 0; i < n; i++){
+        //     mp[nums[i]]++;
+
+        // }
+        // int ans = 0;
+        // for(auto &it : mp){
+        //     if(it.second > n/2){
+        //         ans = it.first;
+        //     }
+        // }
+        // return ans;
+        // second approach
+        int candidate = 0;
+        int count = 0;
+        for(int num : nums){
+            if(count == 0){
+                candidate = num;
+            }
+            if(num == candidate){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return candidate;
+
+        
+    }
+};
