da72110ace2c012e865bd032ef3831bf0c34951d
ayush kumar gubrelay <ayushgubrele4@gmail.com>
2026-06-12 21:50:17 +0530

    Add: contains-duplicate-ii - submission-2

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        // brute force
+        // int n = nums.size();
+        // for(int i = 0; i < n; i++){
+        //     for(int j = i+1; j < n; j++){
+        //         if(nums[i] == nums[j] && abs(i-j) <= k){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+         
+        int n = nums.size();
+        unordered_map<int,int>mp; 
+        for(int i = 0; i < n; i++){
+            if(mp.count(nums[i])){
+                if(mp[nums[i]] != i && abs(mp[nums[i]] - i) <= k){
+                    return true;
+                }
+
+            }
+            mp[nums[i]] = i;
+        }
+        return false;
+
+        
+        
+    }
+};
